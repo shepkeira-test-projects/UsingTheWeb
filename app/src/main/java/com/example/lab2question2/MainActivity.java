c6f213b8135acdc90cce533865124d912999b326
shepkeira <shepherdkeira@gmail.com>
2020-01-24 11:34:35 -0800

    First commit

---
+++ app/src/main/java/com/example/lab2question2/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lab2question2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void webiste(View view) {
+        Uri webpage = Uri.parse("https://library.ok.ubc.ca/");
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void map(View view) {
+        Uri gmmIntentUri = Uri.parse("geo:49.887951,-119.496010");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
+}
